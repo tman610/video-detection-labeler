f79a855c54e4d1b643d865deae8319168829373d
Thomas Manuel <tman610@aucklanduni.ac.nz>
2025-04-05 21:53:19 +1300

    feat: add video seek via seekbar click

--- video_controller.py
+++ video_controller.py
@@ -1,4 +1,5 @@
 from PySide6.QtCore import QTimer
+import time
 
 class VideoController:
     def __init__(self, model, view):
@@ -6,6 +7,7 @@ class VideoController:
         self.view = view
         self.timer = QTimer()
         self.timer.timeout.connect(self._on_timer_timeout)
+        self.last_frame_time = 0
         
         # Connect view signals
         self.view.open_button.clicked.connect(self.open_video)
@@ -14,7 +16,10 @@ class VideoController:
         self.view.next_frame.clicked.connect(self.model.advance_frame)
         self.view.prev_10_frames.clicked.connect(lambda: self.model.seek(self.model.current_frame_index - 10))
         self.view.next_10_frames.clicked.connect(lambda: self.model.seek(self.model.current_frame_index + 10))
-        self.view.seek_slider.sliderMoved.connect(self.model.seek)
+        
+        # Connect slider signals for both dragging and clicking
+        self.view.seek_slider.sliderMoved.connect(self._on_slider_moved)
+        self.view.seek_slider.sliderReleased.connect(self._on_slider_released)
         
         # Connect model signals
         self.model.frame_changed.connect(self.view.display_frame)
@@ -36,12 +41,19 @@ class VideoController:
             frame_rate = self.model.get_frame_rate()
             if frame_rate > 0:
                 self.timer.start(1000 // frame_rate)
+                self.last_frame_time = time.time()
         else:
             self.timer.stop()
     
     def _on_timer_timeout(self):
         """Handle timer timeout for frame advancement"""
-        self.model.advance_frame()
+        current_time = time.time()
+        frame_duration = 1.0 / self.model.get_frame_rate()
+        
+        # Check if it's time for the next frame
+        if current_time - self.last_frame_time >= frame_duration:
+            self.model.advance_frame()
+            self.last_frame_time = current_time
     
     def _on_frame_count_changed(self, frame_count):
         """Handle frame count changes"""
@@ -56,4 +68,13 @@ class VideoController:
         """Handle playback state changes"""
         self.view.update_play_button(is_playing)
         if not is_playing:
-            self.timer.stop() 
+            self.timer.stop()
+    
+    def _on_slider_moved(self, value):
+        """Handle slider movement (dragging)"""
+        self.model.seek(value)
+    
+    def _on_slider_released(self):
+        """Handle slider release (clicking)"""
+        value = self.view.seek_slider.value()
+        self.model.seek(value) 
